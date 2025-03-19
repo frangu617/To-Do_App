4b586b0ee9d76ade62f9d9de70d2618da5dd28e5
Francisco E Gutierrez <142462584+frangu617@users.noreply.github.com>
2025-03-19 10:27:20 -0700

    Added Login.tsx

---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import axios from "axios";
+
+interface LoginProps {
+    setToken: (token: string) => void;
+    api: string;
+}
+
+export default function Login({ setToken, api }: LoginProps) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const login = async () => {
+        try{
+            const res = await axios.post('${api}/login', { username, password });
+            setToken(res.data.access_token);
+
+        } catch (error) {
+            alert('Login Failed!');
+        }
+    };
+
+    return (
+        <div className = "space-y-4">
+            <input value = {username} placeholder="Username"
+            className="border p-2 rounded w-full" 
+            onChange={(e) => setUsername(e.target.value)}
+            />
+            <input value = {password} placeholder="Password" type = "password"
+            className="border p-2 rounded w-full" 
+            onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick = {login} className="bg-blue-500 text-white p-2 rounded w-full">Login</button>
+        </div>
+    );
+}
